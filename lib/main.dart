4631e27194b1e95b79af71f0ca1385a64c3d5e06
Alibrown Julius <brownjulius980@gmail.com>
2025-09-20 23:41:11 +0100

    updated system UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:coding_task/screens/authentication.dart';
 import 'package:coding_task/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ConnectMe',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.themeData,
-      home: const AuthenticationScreen(),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light,
+      child: MaterialApp(
+        title: 'ConnectMe',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.themeData,
+        home: const AuthenticationScreen(),
+      ),
     );
   }
 }
